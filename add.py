bc0262dff527060bad134f67825367aa46373884
Saibollam <sai.bollam10@gmail.com>
2018-02-26 15:08:13 +0530

    Add files via upload
    This is the script to add two numbers using python

---
+++ add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+value_a = input("Please enter the value of a: ")
+value_b = input("Please enter the value of b: ")
+
+addition = value_a + value_b
+
+print "The addition of two numbers are: ", addition
